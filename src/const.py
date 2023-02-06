2b69c271be7242ae6fb4638bd96ef11585ecbb50
saichann <55883274+sai11121209@users.noreply.github.com>
2023-02-06 18:11:03 +0900

    本番用チャンネルコードに変更

--- src/const.py
+++ src/const.py
@@ -19,30 +19,30 @@ class Url:
 
 # チャンネルコード
 class ChannelCode:
-    # 開発用 exception-log チャンネル
-    EXCEPTION_LOG = 1063454659748044863
     # 本番用 exception-log チャンネル
-    # EXCEPTION_LOG = 848999028658405406
+    EXCEPTION_LOG = 848999028658405406
+    # 開発用 exception-log チャンネル
+    # EXCEPTION_LOG = 1063454659748044863
 
+    # 本番用 eft-announcements チャンネル
+    EFT_ANNOUNCEMENTS = 811566006132408340
     # 開発用 announcements-log チャンネル
-    EFT_ANNOUNCEMENTS = 1063454659748044863
-    # 本番用 eft-announcements チャンネル
-    # EFT_ANNOUNCEMENTS = 811566006132408340
+    # EFT_ANNOUNCEMENTS = 1063454659748044863
 
-    # 開発用 readme チャンネル
-    README = 1063454659748044863
     # 本番用 readme チャンネル
-    # README =  890461420330819586
+    README =  890461420330819586
+    # 開発用 readme チャンネル
+    # README = 1063454659748044863
 
-    # 開発用 notification-general チャンネル
-    NOTIFICATION_GNERAL = 1063454659748044863
     # 本番用 notification-general チャンネル
-    # NOTIFICATION_GNERAL =  839769626585333761
+    NOTIFICATION_GNERAL =  839769626585333761
+    # 開発用 notification-general チャンネル
+    # NOTIFICATION_GNERAL = 1063454659748044863
 
-    # 開発用 eft-announcements チャンネル
-    EFT_ANNOUNCEMENTS = 1063454659748044863
     # 本番用 eft-announcements チャンネル
-    # EFT_ANNOUNCEMENTS = 811566006132408340
+    EFT_ANNOUNCEMENTS = 811566006132408340
+    # 開発用 eft-announcements チャンネル
+    # EFT_ANNOUNCEMENTS = 1063454659748044863
 
 class AuthorCode:
     SAI11121209 = 279995095124803595
